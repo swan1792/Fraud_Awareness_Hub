10f3458616970d9947b19bb96ddc31708d3eb288
MyatHtetKyaw <myathtetkyaww@gmail.com>
2026-07-12 17:25:29 +0700

    Add Localization

--- frontend-client/src/main.jsx
+++ frontend-client/src/main.jsx
@@ -2,8 +2,17 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import App from "./App.jsx"
-import "./i18n"
+import i18n from "./i18n"
 import "./index.css"
+
+function syncMeta(lang) {
+  document.documentElement.lang = lang
+  document.title = i18n.t("meta.title")
+  const desc = document.querySelector('meta[name="description"]')
+  if (desc) desc.setAttribute("content", i18n.t("meta.description"))
+}
+i18n.on("languageChanged", syncMeta)
+syncMeta(i18n.language)
 
 const queryClient = new QueryClient({
   defaultOptions: {
